08b02b502adaf0be918a3c454519d1ac6a0c3470
david.danyi <david.danyi92@gmail.com>
2018-02-07 21:32:55 +0100

    Implemented quad constructor

--- structures/quad.py
+++ structures/quad.py
@@ -1,12 +1,47 @@
+import numpy as np
+import math as m
+import utils.math_common as mc
+
+
 class Quad:
 
-    def __init__(self):
-        self.corners = [(), (), (), ()]
+    __round_digits = 10
+
+    def __init__(self, corners):
+        self.corners = tuple(corners)
+
+    @classmethod
+    def create_from_params(cls,
+                           base_length,
+                           b_angle,
+                           c_angle,
+                           b_multiplier,
+                           c_multiplier,
+                           orientation):
+        cos = np.cos([b_angle, np.pi - c_angle])
+        sin = np.sin([b_angle, np.pi - c_angle])
+
+        corners = [(cos[0] * base_length * b_multiplier - base_length / 2,
+                    sin[0] * base_length * b_multiplier),
+                   (-base_length / 2, 0),
+                   (base_length / 2, 0),
+                   (cos[1] * base_length * c_multiplier + base_length / 2,
+                    sin[1] * base_length * c_multiplier)]
+
+        if orientation:
+            rot = mc.create_2d_rotation(orientation)
+            corners = [np.dot(rot, x) for x in corners]
+
+        return cls(corners)
+
+    @classmethod
+    def create_random(cls):
+        return cls(end1=(0, 1), end2=(0, 0), inner1=(1, 0), inner2=(1, 1))
 
 
 def test():
-    quad = Quad()
-    print(type(quad.corners[0]))
+    quad = Quad.create_from_params(4, m.pi / 4, m.pi / 4, m.sqrt(2) / 4, m.sqrt(2) / 4, 0)
+    print(quad.corners)
 
 
 if __name__ == "__main__":
