6d6520aaef0965ab180e07dda2928b1b8099c468
Dávid Danyi D <david.d.danyi@ericsson.com>
2018-02-21 15:07:19 +0100

    Experiment basic generation

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -1,2 +1,8 @@
 """Run RQIM experiments. Placeholder for now"""
+import json
+import lib.structures.quad as quad
+
+quads = [quad.create_random(0.5, 0.2, 1, 0, 2) for i in range(100)]
+file = open("quads.json", "w")
+json.dump(quads, file, cls=quad.QuadEncoder, separators=(',', ':'), indent=4)
 
