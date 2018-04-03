d6f86f125979a5556562ef2c95d8e65f83dd926d
david.danyi <david.danyi92@gmail.com>
2018-04-03 20:50:23 +0200

    Bugfix in detector: base segment selection

--- lib/detectors/detector.py
+++ lib/detectors/detector.py
@@ -3,6 +3,11 @@ import numpy as np
 import lib.structures.basic_geometry as shapes
 import lib.utils.geometry as geom
 
+
+class BaseNotFound(Exception):
+    pass
+
+
 class QuadDetector:
     """Base class for Quad detectors"""
     def detect_quad(self, img):
@@ -10,7 +15,7 @@ class QuadDetector:
 
     @staticmethod
     def _find_corners(lines):
-        pairs = [pair for pair in itertools.combinations(lines, 2)]
+        pairs = [pair for pair in itertools.combinations(lines.tolist(), 2)]
         distances = []
         for pair in pairs:
             l1 = shapes.create_line_segment_from_np(pair[0])
@@ -19,11 +24,16 @@ class QuadDetector:
 
         try:
             pairs.pop(np.argmax(distances))
-            idx = np.in1d(pairs[0], pairs[1])
-            base = shapes.create_line_segment_from_np(np.concatenate(pairs[0])[idx])
+            if pairs[0][0] in pairs[1]:
+                base = shapes.create_line_segment_from_np(pairs[0][0])
+            elif pairs[0][1] in pairs[1]:
+                base = shapes.create_line_segment_from_np(pairs[0][1])
+            else:
+                raise BaseNotFound
+
             line_list = [shapes.create_line_segment_from_np(line) for line in lines.tolist()]
             line_list.remove(base)
-        except ValueError:
+        except BaseNotFound:
             return None
 
         inner = []
